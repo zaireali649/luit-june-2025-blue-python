f6bbf86a1ecbd1889e4858f6aa2455e9ca6d73f8
Zaire Ali <zaireali649@gmail.com>
2025-06-05 19:50:19 -0400

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,37 @@
+number = 151
+number2 = 251
+
+print(number2 - number)
+
+string_of_numbers = "386"
+string_of_numbers2 = "493"
+
+print(string_of_numbers + string_of_numbers2)
+
+print(int(string_of_numbers) + int(string_of_numbers2))
+
+print(type(string_of_numbers))
+
+number3 = 3
+number4 = 9
+number5 = number4 / number3
+
+print(number5)
+
+print(type(number3), type(number4), type(number5))
+
+number6 = int(number5)
+
+print(number6)
+
+string_of_numbers3 = "six four nine"
+
+print(type(string_of_numbers3))
+print(dir(string_of_numbers3))
+
+print(string_of_numbers3.capitalize())
+print(string_of_numbers3.upper())
+print(string_of_numbers3.title())
+
+print(string_of_numbers3.isalnum())
+print(string_of_numbers.isalnum())
