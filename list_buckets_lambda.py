210b5d4ce4b85aa40a0dfb83409348b737c7254d
Zaire Ali <zaireali649@gmail.com>
2025-06-19 19:32:20 -0400

    Update list_buckets_lambda.py

--- list_buckets_lambda.py
+++ list_buckets_lambda.py
@@ -1,16 +1,17 @@
 import json
-import boto3
+import boto3  # Import the boto3 library to interact with AWS services
 
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    
-    response = s3.list_buckets()
+    s3 = boto3.client('s3')  # Create an S3 client using the default AWS credentials and config
+
+    response = s3.list_buckets()  # Call the list_buckets API to get all S3 buckets in the account
 
     bucket_names = []
-    buckets = response["Buckets"]
-    
-    for bucket in buckets:
-        print(bucket["Name"])
+
+    buckets = response["Buckets"]  # Extract the list of bucket dictionaries from the response
+
+    for bucket in buckets:  # Iterate through each bucket in the list
+        print(bucket["Name"])  # Print the name of each S3 bucket
         bucket_names.append(bucket["Name"])
     
     return {
